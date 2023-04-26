469bef255e1d77437171f7fa97a7c534a6f818ba
Jake Garcia <bballwiz4@gmail.com>
2023-04-25 18:21:08 -0700

    Added in all needed inputs, select and textarea. Gave the select box the logic needed to function.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -18,9 +18,32 @@ const Contact = () => {
     const [costPerson, setCostPerPerson] = useState("");
     const [comments, setComments] = useState("");  
 
+    const renderOptions = () => {
+        return packages.map((packageText, i) => <option value={i} key={`${i}-${packageText}`} >{packageText}</option>)
+    }
+
+    const handleFoodPackageSelection = e => {
+        let {value} = e.target;
+        setPackageIndex(parseInt(value));
+        setFoodPackage(packages[value]);
+    }
+
     return (
         <div id="contact" className="w-full bg-white rounded text-black mb-4 py-12 px-4">
             <SectionTitle text={"Contact"} />
+            <input value={fName} type="text" placeholder="First Name"/>
+            <input value={lName} type="text" placeholder="Last Name" />
+            <input value={email} type="email" placeholder="Email"/>
+            <input value={phone} type="tel" placeholder="Phone Number"/>
+            <input value={eventDate} type="date" placeholder="Event Date"/>
+            <input value={city} type="text" placeholder='City'/>
+            <input value={numberOfGuests} type="number" placeholder='# of Guests'/>
+            <select onChange={handleFoodPackageSelection} placeholder='Pick a Package'>
+                <option>Pick a Package</option>
+                {renderOptions()}
+            </select>
+            <input value={costPerson} type="number" placeholder='$ per Person'/>
+            <textarea value={comments} onChange={(e) => setComments(e.target.value)}>{comments}</textarea>
         </div>
     )
 }
