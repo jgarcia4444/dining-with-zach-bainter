1d16d75d0de380ae09b7bd1cd9933462db00d1db
Jake Garcia <bballwiz4@gmail.com>
2023-04-26 13:26:30 -0700

    Styled the contact form to have a gray bg. Also made the inputs into their own components. Added icons and labels to the inputs. Added needed logic to configure labels, icons and types.

---
+++ src/components/Contact/InputComponent/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {FiUser, FiLock, FiMail, FiPhone, FiPackage, FiDollarSign, FiCalendar, FiGlobe, FiUsers} from 'react-icons/fi';
+
+const InputComponent = ({inputInfo}) => {
+    const {label, value, changeFunction, type} = inputInfo;
+    const packages = ["French", "Fine Dining", "Mediterranean", "Italian", "BBQ", "Mexican", "Cocktail Party"];
+
+    const userInput = () => {
+        let input;
+        if (type === "textarea") {
+            input = (
+                <textarea placeholder='Any comments or special requests?' className="w-full rounded p-2 mb-4" value={value} onChange={changeFunction}>{value}</textarea>
+            )
+        } else if (type === "package") {
+            input = (
+                <select className="w-full rounded p-2 mb-4" onChange={changeFunction} placeholder={label}>
+                    <option>{label}</option>
+                    {renderOptions()}
+                </select>
+            )
+        } else {
+            input = (
+                <input onChange={changeFunction} className="w-full rounded p-2" value={value} type={configureType()} placeholder={label} />
+            )
+        } 
+        return input;
+    }
+
+    const configureIcon = () => {
+        let size = 24;
+        let color = "#ccc"
+        switch(type) {
+            case "user":
+                return <FiUser color={color} size={size} />;
+            case "pass":
+                return <FiLock color={color} size={size} />;
+            case 'email':
+                return <FiMail color={color} size={size} />;
+            case 'phone':
+                return <FiPhone color={color} size={size}/>
+            case 'package':
+                return <FiPackage color={color} size={size}/>
+            case 'budget':
+                return <FiDollarSign color={color} size={size}/>;
+            case 'date':
+                return <FiCalendar color={color} size={size}/>;
+            case 'location':
+                return <FiGlobe color={color} size={size}/>;
+            case 'number':
+                return <FiUsers color={color} size={size}/>
+            default:
+                return <FiUser color={color} size={size} />;
+        }
+    }
+
+    const configureType = () => {
+        switch(type) {
+            case "user":
+                return "text";
+            case "email":
+                return "email";
+            case "phone": 
+                return "tel";
+            case "date":
+                return "date";
+            case 'number':
+                return 'number'
+            case 'budget':
+                return 'number';
+            default:
+                return "text"
+        }
+    }
+
+    const renderOptions = () => {
+        return packages.map((packageText, i) => <option value={i} key={`${i}-${packageText}`} >{packageText}</option>)
+    }
+
+    return (
+        <div className="w-1/2 p-4 flex flex-col justify-center items-center transition-all">
+            { value !== "" &&
+                (<div className={`w-full bg-white rounded-b-none  z-50 rounded ${value !== "" ? "pb-2" : ""}`}>
+                    <label className="text-gray-300 pl-2 " >{value === "" ? "" : label}</label>
+                </div>)
+            }
+            <div className={`w-full flex flex-row items-center justify-center px-2 rounded  bg-white ${value !== "" ? "rounded-t-none" : ""}`}>
+                <div className="flex items-start justify-center">
+                    {configureIcon()}
+                </div>
+                <div className="w-full">
+                    {userInput()}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default InputComponent;
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,7 @@
 
 import React, {useState} from 'react'
 import SectionTitle from '../../shared/SectionTitle'
+import InputComponent from './InputComponent';
 
 const Contact = () => {
 
@@ -18,33 +19,34 @@ const Contact = () => {
     const [costPerson, setCostPerPerson] = useState("");
     const [comments, setComments] = useState("");  
 
-    const renderOptions = () => {
-        return packages.map((packageText, i) => <option value={i} key={`${i}-${packageText}`} >{packageText}</option>)
-    }
-
     const handleFoodPackageSelection = e => {
         let {value} = e.target;
         setPackageIndex(parseInt(value));
         setFoodPackage(packages[value]);
     }
 
+    let inputs = [
+        {value: fName, changeFunction: e => setFName(e.target.value), label: "First Name", type: "user"},
+        {value: lName, changeFunction: e => setLName(e.target.value), label: "Last Name", type: "user"},
+        {value: email, changeFunction: e => setEmail(e.target.value), label: "Email", type: "email"},
+        {value: phone, changeFunction: e => setPhone(e.target.value), label: "Phone Number", type: "phone"},
+        {value: eventDate, changeFunction: e => setEventDate(e.target.value), label: "Event Date", type: "date"},
+        {value: city, changeFunction: e => setCity(e.target.value), label: "Event City", type: "location"},
+        {value: numberOfGuests, changeFunction: e => setNumberOfGuests(e.target.value), label: "Number of Guests", type: "number"},
+        {value: foodPackage, changeFunction: handleFoodPackageSelection, label: "Pick a Package", type: "package"},
+        {value: costPerson, changeFunction: e => setCostPerPerson(e.target.value), label: "Cost per Person", type: "budget"},
+        {value: comments, changeFunction: e => setComments(e.target.value), label: "Comments", type: "textarea"}
+    ]
+
+    const renderInputs = () => {
+        return inputs.map((inputInfo, i) => <InputComponent inputInfo={inputInfo} key={`${i}-${inputInfo.label}`} />)
+    }
+
     return (
         <div id="contact" className="w-full bg-white rounded text-black mb-4 py-12 px-4">
             <SectionTitle text={"Contact"} />
-            <div className="flex flex-row flex-wrap justify-between items-center mt-4 rounded shadow p-2 pt-4 bg-black relative">
-                <input onChange={e => setFName(e.target.value)} className="w-2/5 rounded p-2 mb-4" value={fName} type="text" placeholder="First Name"/>
-                <input onChange={e => setLName(e.target.value)} className="w-2/5 rounded p-2 mb-4" value={lName} type="text" placeholder="Last Name" />
-                <input onChange={e => setEmail(e.target.value)} className="w-2/5 rounded p-2 mb-4" value={email} type="email" placeholder="Email"/>
-                <input onChange={e => setPhone(e.target.value)} className="w-2/5 rounded p-2 mb-4" value={phone} type="tel" placeholder="Phone Number"/>
-                <input onChange={e => setEventDate(e.target.value)} className="w-2/5 rounded p-2 mb-4" value={eventDate} type="date" placeholder="Event Date"/>
-                <input onChange={e => setCity(e.target.value)} className="w-2/5 rounded p-2 mb-4" value={city} type="text" placeholder='City'/>
-                <input onChange={e => setNumberOfGuests(e.target.value)} className="w-2/5 rounded p-2 mb-4" value={numberOfGuests} type="number" placeholder='# of Guests'/>
-                <select className="w-2/5 rounded p-2 mb-4" onChange={handleFoodPackageSelection} placeholder='Pick a Package'>
-                    <option>Pick a Package</option>
-                    {renderOptions()}
-                </select>
-                <input onChange={e => setCostPerPerson(e.target.value)} className="w-2/5 rounded p-2 mb-4" value={costPerson} type="number" placeholder='$ per Person'/>
-                <textarea placeholder='Any comments or special requests?' className="w-2/5 rounded p-2 mb-4" value={comments} onChange={(e) => setComments(e.target.value)}>{comments}</textarea>
+            <div className="flex flex-row flex-wrap justify-between items-start mt-4 rounded shadow p-2 pt-4 bg-gray-300 relative">
+                {renderInputs()}
                 <div className="w-full border-2 border-white rounded text-white text-center py-2">
                     Send Form
                 </div>
