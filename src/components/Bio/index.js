166f4a5011707286cf9f25d6c8a916c69944b31a
Jake Garcia <bballwiz4@gmail.com>
2023-05-04 11:35:22 -0700

    Updated the verbage of the bio text

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -17,14 +17,14 @@ const Bio = () => {
                 </div>
                 <div className="w-full">
                     <p className=" text-gray-700">
-                    Hello and welcome to my website! My name is Zach Bainter, and I am a passionate chef, gardener, and farmer with over 20 years of experience in the culinary industry. As an excellent host, I love to bring people together over delicious food and create unforgettable dining experiences.<br /><br />
-In addition to my culinary expertise, I am also a professional director and photographer specializing in food and chef stories. I love to capture the beauty of food and the stories behind the people who create it. <br /><br />
+                    Hello and welcome to my website! My name is Zach Bainter and I am a passionate chef, gardener, and former farmer with over 20 years of experience in the culinary world. As an excellent host, I love to bring people together over delicious food and create unforgettable dining experiences. <br /><br />
+                    In addition to my culinary expertise, I am also a professional director and photographer specializing in food and chef stories. I love to capture the beauty of food and the stories behind the people who create it. <br /><br />
 
-As an event services specialist, I have the skills and experience necessary to bring your vision to life. Whether you are hosting a small dinner party or a large corporate event, I can provide customized menus, beautiful decor, and exceptional service to ensure your event is a success. <br /><br />
+                    As an event services specialist, I have the skills and experience necessary to bring your vision to life. Whether you are hosting a small dinner party or a corporate event, I can provide customized menus, beautiful decor, and exceptional service to ensure your event is a success.  <br /><br />
 
-I am a social person who enjoys connecting with others over shared interests, and I believe that food is a universal language that brings people together. When I'm not in the kitchen or tending to my garden and farm, you can find me exploring new culinary trends, meeting with fellow food enthusiasts, and sharing my love of food with others. <br /> <br />
+                    I am a social person who enjoys connecting with others over shared interests, and I believe that food is a universal language that brings people together. When I'm not in the kitchen or tending to my garden, you can find me exploring new culinary trends, meeting with fellow food enthusiasts, and sharing my love of food with others.  <br /> <br />
 
-Thank you for visiting my website, and I look forward to sharing my culinary journey with you!
+                    Thank you for visiting my website, and I look forward to sharing my culinary journey with you!
                     </p>
                 </div>
             </div>
